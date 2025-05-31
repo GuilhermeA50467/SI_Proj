8a4cab3ddfdba2dbfe3de49b17e329dac6f0231c
GuilhermeA50467 <A50467@alunos.isel.pt>
2025-05-31 18:52:34 +0100

    updates

--- scooter/scooter/cites-project/dal/src/main/java/isel/sisinf/jpa/ServicesData/DataDock.java
+++ scooter/scooter/cites-project/dal/src/main/java/isel/sisinf/jpa/ServicesData/DataDock.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class DataDock {
 
-    private static String inputData(String prompt, boolean show) {
+    private static String inputData(String str, boolean show) {
         Scanner key = new Scanner(System.in);
-        if (show) System.out.print("Enter corresponding values:\n");
-        System.out.print(prompt);
+        if (show) System.out.print("Enter corresponding values, separated by commas of:\n");
+        System.out.print(str);
         return key.nextLine();
     }
 
     public static int[] readDockScooterInput() {
-        int dockId = Integer.parseInt(inputData("Dock ID", false));
-        int scooterId = Integer.parseInt(inputData("Scooter ID", false));
+        int dockId = Integer.parseInt(inputData("Dock ID:", false));
+        int scooterId = Integer.parseInt(inputData("Scooter ID:", false));
         return new int[] { dockId, scooterId };
     }
 }
